debfd2e26e3ab2451f7f501264ea3a4fc3c66757
Darren Cunningham <darren.cunningham@me.com>
2012-08-15 22:00:14 -0400

    attempting to change the initializtion to not need the require statement

--- webnotify.js
+++ webnotify.js
@@ -4,11 +4,11 @@
   * copyright Darren Cunningham 2012
   * MIT License
   */
-(function (name, definition, context) {
-  if (typeof module != 'undefined' && module.exports) module.exports = definition()
-  else if (typeof context['define'] == 'function' && context['define']['amd']) define(name, definition)
-  else context[name] = definition()
-})('webnotify', function () {
+!function (name, definition) {
+  if (typeof define == 'function') define(definition)
+  else if (typeof module != 'undefined') module.exports = definition()
+  else this[name] = definition()
+}('webnotify', function () {
   if (!Notification) return;
 
   var context = this,
@@ -28,7 +28,7 @@
     }
   };
 
-  var webnotify = function (title, options)
+  function webnotify(title, options)
   {
     context.title = title;
     context.options = (options === undefined) ? {} : options;
@@ -37,4 +37,4 @@
   };
 
   return webnotify;
-}, this);
+});
